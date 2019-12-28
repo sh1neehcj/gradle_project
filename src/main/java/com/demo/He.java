5e0bd8ea71087ed25181676833c058ead287022d
sh1neehcj <shineehcj@126.com>
2019-12-28 11:50:37 +0800

    复制demo01的文件,并删除demo.java,测试删除文件

---
+++ src/main/java/com/demo/He.java
@@ -0,0 +1,13 @@
+/**
+ * Author: shineehcj
+ * Date: 10:54  12/28/2019
+ * Motto:love Java,love Python and Enjoy Life...
+ */
+
+package com.demo;
+
+public class He {
+    public static void main(String[] args) {
+        System.out.println("he");
+    }
+}
