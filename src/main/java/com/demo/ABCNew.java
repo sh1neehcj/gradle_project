5be727f972797dde9193377bebfb113aee638c70
Mr.He <50021066+sh1neehcj@users.noreply.github.com>
2019-12-28 12:03:05 +0800

    aaa

--- src/main/java/com/demo/ABCNew.java
+++ src/main/java/com/demo/ABCNew.java
@@ -13,7 +13,7 @@ import java.io.FileReader;
 public class ABCNew {
     public static void main(String[] args) {
         try {
-            FileReader fr = new FileReader(new File("D:\\111\\user3.txt"));
+            FileReader fr = new FileReader(new File("D:\\111\\aaa.txt"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
